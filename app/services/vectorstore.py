45ead2d4ffd09134c27569aae57e6af008a6ca14
Md. Mahmudul Hasan Riyad <mhriyad98@gmail.com>
2024-05-15 16:02:18 +0600

    Mon_May14_2024: rewrite vestore store

--- app/services/vectorstore.py
+++ app/services/vectorstore.py
@@ -8,25 +8,22 @@ from langchain_community.vectorstores import Chroma
 
 
 class DocVectorStore:
-    vector_store: Union[Callable[..., Chroma], None] = None
 
-    @classmethod
-    def load_store(cls, path=DOC_PATH, chunk_size=500, chunk_overlap=50) -> Chroma:
+    @staticmethod
+    def load_vector_store(path=VECTOR_DB_PATH) -> Chroma:
         embedding = OllamaEmbeddings(model=MODEL)
-        if cls.vector_store is None:
-            cls.vector_store = Chroma(persist_directory="./chroma",
-                                      embedding_function=embedding)
+        vector_store = Chroma(persist_directory=VECTOR_DB_PATH, embedding_function=embedding)
 
-    @classmethod
-    def create_store(cls, path=DOC_PATH, chunk_size=500, chunk_overlap=50, embedding_model=MODEL):
+        return vector_store
+
+    @staticmethod
+    def build_vector_store(path=DOC_PATH, chunk_size=500, chunk_overlap=50,
+                           embedding_model=MODEL):
         loader = TextLoader(file_path=path)
         text = loader.load()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
                                                        chunk_overlap=chunk_overlap)
         splits = text_splitter.split_documents(text)
         embeddings = OllamaEmbeddings(model=embedding_model)
-        vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings,
-                                            persist_directory=VECTOR_DB_PATH)
-        cls.vector_store = vectorstore
-
-
+        Chroma.from_documents(documents=splits, embedding=embeddings,
+                              persist_directory=VECTOR_DB_PATH)
