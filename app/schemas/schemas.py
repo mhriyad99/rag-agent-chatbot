fd46edf40afdc5abdaf2ad03ae426bb8b0f8bb7c
Md. Mahmudul Hasan Riyad <mhriyad98@gmail.com>
2024-05-15 15:44:21 +0600

    Mon_May14_2024: create tools.py and add GraphState to schemas.py

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,6 +1,24 @@
+from typing import List
+
 from fastapi import UploadFile
 from pydantic import BaseModel
 
 
 class Query(BaseModel):
     query: str
+
+
+class GraphState(BaseModel):
+    """
+        Represents the state of our graph.
+
+        Attributes:
+            question: question
+            generation: LLM generation
+            web_search: whether to add search
+            documents: list of documents
+    """
+    question: str
+    generation: str
+    web_search: str
+    documents: List[str]
